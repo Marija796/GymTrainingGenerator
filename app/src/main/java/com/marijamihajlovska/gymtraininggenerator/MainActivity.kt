c70d9c49758dcf8a73ced6b9ed142a77660646e8
Marija796 <121623159+Marija796@users.noreply.github.com>
2026-05-24 17:46:56 +0200

    Phase 8 - Firebase Analytics complete

--- app/src/main/java/com/marijamihajlovska/gymtraininggenerator/MainActivity.kt
+++ app/src/main/java/com/marijamihajlovska/gymtraininggenerator/MainActivity.kt
@@ -5,15 +5,21 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.marijamihajlovska.gymtraininggenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAnalytics = Firebase.analytics
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -22,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView?.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
+            val bundle = Bundle()
+            bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, destination.label.toString())
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
+
             when (destination.id) {
                 R.id.loginFragment, R.id.registerFragment -> {
                     binding.bottomNavigationView?.visibility = View.GONE
